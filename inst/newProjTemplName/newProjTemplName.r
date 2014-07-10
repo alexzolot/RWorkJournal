92cf8c4cdf94c5f6bafd7725a658ceee23adcbf1
Alex Zolot <ale@zolot.us>
2014-07-10 01:11:56 -0700

    Update newProjTemplName.r

--- inst/newProjTemplName/newProjTemplName.r
+++ inst/newProjTemplName/newProjTemplName.r
@@ -1,7 +1,7 @@
-#! /usr/bin/Rscript --vanilla --slave
+#! /usr/bin/Rscript
 # Project  : newProjTemplName
 # File     : newProjTemplName/newProjTemplName.r
-# Author   : Alex Zolotoviski, azolotovitski@medio.com
+# Author   : Alex Zolotoviski, alex@zolot.us
 # Created  : 00-00-00
 # License  : GPL-2
 ###############################################################################
@@ -9,14 +9,19 @@
 { #== init ===
 	rm(list=ls(envir = .GlobalEnv), envir = .GlobalEnv)
 	
-	options(help='html', digits=4, width=2160, scipen=5, editor='C:\\Program Files (x86)\\Notepad++\\notepad++.exe'
-			, error= NULL) # options(error= recover) options(error=dump)  #or options(error=NULL)
+	options(help='html', digits=4, width=2160, scipen=5, editor='C:\\Program Files (x86)\\Notepad++\\notepad++.exe')
+	options(error= NULL) # options(error= recover) options(error=dump)
+	options(datatable.print.nrows=200)
+
 
 	onWin= Sys.getenv('R_PLATFORM')==''
-	if(onWin){root= 'T:/work/UseR-2013'; 	memory.limit(size=9000)}  else root= '/home/azolotovitski/work'   # memory.limit()
+	if(onWin){root= 'M:'; 	memory.limit(size=9000)}  else root= '/home/azolotovitski/work'   # memory.limit()
+	
+	library(HaLaP)  # it loads libras SOAR, data.table
+	library(RWorkJournal)
 	
 	#source(file.path(root, 'lib/zBase0.r'))  
-	source('T:/work/UseR-2013/lib/zBase0.r')  # xxx: hard coded path to zBase.r
+	source('T:/work/UseR-2013/lib/zBase0.r')  # or hard coded paths to source files
 	source('T:/work/UseR-2013/lib/zBase1.r')  
 	source('T:/work/UseR-2013/lib/zCode.r')  
 	source('T:/work/UseR-2013/lib/zStats.r')  
@@ -34,6 +39,8 @@
 	#	setDefaults(symbols, inc=.15)
 	#libra(randomForest)
 	#libra(plotrix)  # addtable2plot
+	#libra(Cairo);  
+
 	
 	#libra(ggplot2)
 	#libra(scales)
@@ -42,38 +49,37 @@
 	proot= fp(root, 'newProjTemplName')  # project root
 	sw(fp(proot, 'out'))
 	
-	#libra(Cairo);  
 		
 	theFile= fp(proot, 'newProjTemplName.r')
 	
-	HHp.bak= HHp #; HHp=dummy; # HHp= HHp.bak   # to rerun w/o change images
-	sa.bak= sa   #; sa=dummy ; # sa= sa.bak   # to rerun w/o change images
-
-	
-	# catf('\ndetach("%s",  character.only = TRUE)', grep('^pac', search(), v=T))
-	# gff('saved', theFile)
-	# rmDF(); loo(); lo(); lsDF();
+        options(theFile= theFile)
+	sg.bak= sg    # ; sg=dummy ; # sa= sa.bak   # to rerun w/o change images
+	sgg.bak= sgg  # ; sgg=dummy; # sa= sa.bak   # to rerun w/o change images
+	sa.bak= sa    # ; sa=dummy ; # sa= sa.bak   # to rerun w/o change images
+	saa.bak= saa  # ; saa=dummy; # sa= sa.bak   # to rerun w/o change images
+					
+	# rmDF(); gff('saved'); loo(); lo(); lsDF(); dett(); gw(); taa(); loo()
 } #--
 ##########################################################
 
-#====   newProjTemplName  ====
+#   newProjTemplName  
 
-if(0){#== Data Inventory  ==
+if(0){###  Data Inventory  ==
 	gw() 
 	dir() # expl() 
 }
 
-{#== Data Exploration  ==
+if(0){###  Data Exploration   ==
 }
 
-{#== Predictive Modeling ==
+if(0){###  Predictive Modeling  ==
 }
 
-{#== Reports ==
+if(0){###  Reports  ==
 }
 
 
-if(0){   #== Misc
+if(0){###  Misc
 	theFile= 'm:/newProjTemplName/newProjTemplName.r'
 	gff('saved', theFile)
 	gff('sa\\(|===', theFile)
@@ -81,5 +87,5 @@ if(0){   #== Misc
 	theFile= fp(proot, 'newProjTemplName.r')
 	ccc= function()code2HTML(theFile)
 	
-	CreateNewProj(newProj.name= 'zzz', Templ.dir= 'T:/work/UseR-2013/lib/newProjTemplNa me', root='T:/work/UseR-2013')
+	CreateNewProj(newProj.name= 'zzz', Templ.dir= 'M:/lib/newProjTemplNam e', root='M:')  # Fix "Nam e" !!!
 } #--
